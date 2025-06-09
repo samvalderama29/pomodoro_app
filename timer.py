130e2c258b41fbeb3ce1b20bdfa50c3c9b5ec183
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-09 19:00:12 +0800

    feat: implement two functions under the Timer class; def start() to launch timer in a new thread and def run_timer() to handle session logic and countdown

--- timer.py
+++ timer.py
@@ -9,3 +9,24 @@ class Timer:
         self.reps = 0
         self.running = False
         self.thread = None
+
+    def start(self):
+        if not self.running:
+            self.running = True
+            self.reps += 1
+            self.thread = threading.Thread(target = self.run_timer)
+            self.thread.start()
+
+    def run_timer(self):
+        if self.reps % 8 == 0:
+            seconds = long_break_min * 60
+        elif self.reps % 2 == 0:
+            seconds = short_break_min * 60
+        else:
+            seconds = work_min * 60
+
+        while seconds > 0 and self.running:
+            mins, secs = divmod(seconds, 60)
+            self.on_update(f"{mins:02}:{secs:02}")
+            time.sleep(1)
+            seconds -= 1

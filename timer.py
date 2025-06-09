d9b8e79b2b2ef28802b459288e784f13fc049956
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-09 18:49:06 +0800

    feat: add Timer class with constructor to initialize timer state

--- timer.py
+++ timer.py
@@ -2,3 +2,10 @@ import time
 import threading
 from configurations import work_min, short_break_min, long_break_min
 
+class Timer:
+    def __init__(self, on_update, on_session_complete):
+        self.on_update = on_update
+        self.on_session_complete = on_session_complete
+        self.reps = 0
+        self.running = False
+        self.thread = None

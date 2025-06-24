0050dbd9320743507fd14055c3d632a7c1c11905
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-24 09:53:33 +0800

    feat: create a function to set up the window tab with a title and logo

--- ui_layout.py
+++ ui_layout.py
@@ -11,3 +11,9 @@ class PomodoroUI:
         self.setup_background()
         self.create_widgets()
         self.place_widgets()
+
+    def setup_window(self):
+        self.root.title("Pomodoro Trakcer")
+        self.root.geometry("800x450")
+        self.root.resizable(False, False)
+        self.root.iconphoto(False, tk.PhotoImage(file = "pomodoro_logo.png"))

1f8e17d4b81f4fc1407991a1fbef00463a5139b4
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-24 12:11:57 +0800

    feat: create PomodoroApp class in main to initialize and run the timer and UI

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import tkinter as tk
 from timer import Timer
 from ui_layout import PomodoroUI
+
+class PomodoroApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.timer = Timer(self.update_display, self.session_complete)
+        self.ui = PomodoroUI(self.root, self.timer)
+
+    def update_display(self, time_str):
+        self.ui.update_display(time_str)
+
+    def session_complete(self):
+        self.ui.session_complete()
+
+    def run(self):
+        self.root.mainloop()

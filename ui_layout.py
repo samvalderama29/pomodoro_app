3242bdf812d4de8d5e5f65b0ee14ea635de795a0
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-24 09:58:18 +0800

    feat: create a function to set up fonts for buttons and labels

--- ui_layout.py
+++ ui_layout.py
@@ -17,3 +17,10 @@ class PomodoroUI:
         self.root.geometry("800x450")
         self.root.resizable(False, False)
         self.root.iconphoto(False, tk.PhotoImage(file = "pomodoro_logo.png"))
+
+    def setup_fonts(self):
+        display_font = "Montserrat" if "Montserrat" in font.families() else "Helvetica"
+        self.primary_font = (display_font, 18)
+        self.label_font = (display_font, 24, "bold")
+        self.timer_font = (display_font, 83, "bold")
+        self.button_font = (use_font, 12)

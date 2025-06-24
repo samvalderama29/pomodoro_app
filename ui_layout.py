54146747ed53d62c7a6c9a881c77d8bdb2f34abd
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-06-24 09:50:24 +0800

    feat: create PomodoroUI class to initialize window, fonts, background, and widgets for main display and timer-based actions

--- ui_layout.py
+++ ui_layout.py
@@ -1,2 +1,13 @@
 import tkinter as tk
 from tkinter import font
+
+class PomodoroUI:
+    def __init__(self, root, timer):
+        self.root = root
+        self.timer = timer
+
+        self.setup_window()
+        self.setup_fonts()
+        self.setup_background()
+        self.create_widgets()
+        self.place_widgets()
